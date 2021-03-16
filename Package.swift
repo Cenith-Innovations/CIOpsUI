29ff87c622cfc55de824aefa23ce74ca10f4ba39
Elmo <elmo5676@gmail.com>
2021-03-16 12:08:09 -0500

    Got the TAF and METAR right I think. needs to be tested though

--- Package.swift
+++ Package.swift
@@ -11,10 +11,7 @@ let package = Package(
             name: "CIOpsUI",
             targets: ["CIOpsUI"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [.package(url: "https://github.com/Cenith-Innovations/CIFoundation", .upToNextMajor(from: "1.0.0"))],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
